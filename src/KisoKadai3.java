6177513beb9a67a2575b6376d827a6dbe0ca1558
='Yurina <='yurinanoaddress.0425@i.softbank.jp'>
2016-01-25 16:14:33 +0900

    2016/01/25KisoKadai3修正

--- src/KisoKadai3.java
+++ src/KisoKadai3.java
@@ -26,22 +26,24 @@ public class KisoKadai3{
 
 
 
-
+			//4を押すと終了する
 			while(x!=4){
-				System.out.println("このプログラムは/1新規/2書き込む/3読む/4終了/をすることができます。");
+				System.out.println("/1ファイルの作成/2書く/3読む/4終了/5追記/");
 				x= new java.util.Scanner (System.in).nextInt();
 
-
+			//1を押すとファイルを作成する
 				if(x==1){
 					System.out.println("ファイルを作成します。ファイル名を入力してください(例)C:\\Users\\internous\\Desktop\\新JavaScript演習\\abc.txt");
 					methodA();
 				}
 
 
-
+			//2を押すと書き込む
 				if(x==2){
-					System.out.println("ファイルに書き込みます。ファイルがなければ新規作成します。編集したいファイル名を入力してください。C:\\Users\\internous\\Desktop\\新JavaScript演習\\abc.txt");
+					System.out.println("ファイルに書き込みます。ファイルがなければ新規作成します。編集したいファイル名を入力してください。例C:\\Users\\internous\\Desktop\\新JavaScript演習\\abc.txt");
 						methodB();
+
+
 				}
 
 
@@ -51,9 +53,18 @@ public class KisoKadai3{
 
 
 				if(x==3){
-					System.out.println("ファイルを開きます。ファイル名を入力してください");
+					System.out.println("ファイルを開きます。ファイル名を入力してください.");
 						methodC();
 				}
+
+
+				if(x==5){
+					System.out.println("追記したいファイル名を入力してください");
+					methodD();
+				}
+
+
+
 			}
 
 
@@ -135,6 +146,11 @@ public class KisoKadai3{
 				String name=new java.util.Scanner(System.in).nextLine();
 					System.out.println("書き込んでください");
 
+
+
+
+
+
 						try{
 							File b=new File(name);
 
@@ -143,14 +159,16 @@ public class KisoKadai3{
 							boolean a=b.exists ();
 
 								if(a==true){
-									PrintWriter p = new PrintWriter(new BufferedWriter(new FileWriter(name)));
+									PrintWriter p = new PrintWriter(new BufferedWriter(new FileWriter(name,true)));
+
+
 
 									p.println(pw);
 
 
 									p.close();
 
-								    System.out.println("書き込みました");
+								    System.out.println("上書しました");
 
 								}else{
 
@@ -179,6 +197,7 @@ public class KisoKadai3{
 					boolean a=b.exists ();{
 						if(a==true){
 							System.out.println("読み込みました");
+
 
 
 							}
@@ -217,19 +236,53 @@ public class KisoKadai3{
 
 
 
+
+
+
+
 					}
 
 			}
 
 
+			public static void methodD(){
+
+				String name=new java.util.Scanner(System.in).nextLine();
+				System.out.println("追記する文章を入力してください。例C:\\Users\\internous\\Desktop\\新JavaScript演習\\abc.txt");
+
+
+				try{
+					File b=new File(name);
+
+					String pw=new java.util.Scanner(System.in).nextLine();
+
+					boolean a=b.exists ();
+
+						if(a==true){
+							PrintWriter p = new PrintWriter(new BufferedWriter(new FileWriter(name,true)));
+
+
+
+							p.println(pw);
+
+
+							p.close();
+
+						    System.out.println("追記しました");
+
+						}else{
+
+							System.out.println("ファイルに書き込めません");
+						}
+				}catch(IOException e){
+		    	  System.out.println(e);
+				}
 
 
 
 
 
-
-
-
+			}
 
 
 
